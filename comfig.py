ed5475f867f3c82b4173cca02af1e9a408b8cbb6
migueanto <102918404+migueanto@users.noreply.github.com>
2022-04-03 17:51:49 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = '5167422266:AAF8MMMusWeE-PnTl9mAUbLMaFFXP7pNMn8'
 API_ID = '14443740'
-API_HASH = ''
-ADMIN_ID = [Migulluco]
+API_HASH = '5c69586e7822b5c9c8db5f0a93dd7ac6'
+ADMIN_ID = [1522526895]
 
 MOODLE_URL = 'https://evea.uh.cu/'
 MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
